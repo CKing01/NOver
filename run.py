425081c2bfc21f67d8017b144c4fa220b47bde59
Shiva <150210693+CKing01@users.noreply.github.com>
2023-11-08 09:51:13 +0000

    base ready

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
 # os.system("python3 manage.py makemigrations")
 # os.system("python3 manage.py migrate")
-os.system("pip3 install -U g4f")
+os.system("pip install -U g4f")
 os.system("gunicorn NOverBackend.wsgi")
