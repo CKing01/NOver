b240f1c2455f3750c81aeb74ca29e4674a56a5d7
Shiva <150210693+CKing01@users.noreply.github.com>
2023-11-08 10:33:43 +0000

    base ready

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
 os.system("/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip")
 os.system("pip install -U g4f")
-os.system("python manage.py makemigrations")
-os.system("python manage.py migrate")
+os.system("python manage.py makemigrations NOverBackend")
+os.system("python manage.py migrate NOverBackend")
 # os.system("python3 manage.py collectstatic")
 os.system("ls")
 os.system("gunicorn NOverBackend.wsgi")
