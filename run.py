962965ce9356edd60f3f9be851a734f99f02d7f6
Shiva <150210693+CKing01@users.noreply.github.com>
2023-11-08 15:16:10 +0000

    base ready

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 import os
 os.system("/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip")
 os.system("pip install -U g4f")
-os.system("python manage.py makemigrations NOverBackend")
-os.system("python manage.py migrate NOverBackend")
-# os.system("python3 manage.py collectstatic")
-os.system("ls")
-os.system("gunicorn NOverBackend.wsgi")
+import g4f
+response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello world code in python"}],
+    stream=True,
+)
+
+for message in response:
+    print(message, flush=True, end='')
+# os.system("python manage.py makemigrations NOverBackend")
+# os.system("python manage.py migrate NOverBackend")
+# # os.system("python3 manage.py collectstatic")
+# os.system("ls")
+# os.system("gunicorn NOverBackend.wsgi")
